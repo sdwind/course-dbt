21b455576fa066e0c2d01dce24087e4883db0530
Suzanne Windhorst <sdwind@gmail.com>
2024-10-14 22:52:48 +0000

    Week 1 assignment

---
+++ greenery/models/staging/postgres/stg_postgres__addresses.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='table') }}
+
+SELECT 
+    address_id,
+    address,
+    zipcode,
+    state,
+    country
+FROM {{ source('greenery', 'addresses') }}
---
+++ greenery/models/staging/postgres/stg_postgres__promos.sql
@@ -0,0 +1,7 @@
+{{ config(materialized='table') }}
+
+SELECT 
+    promo_id,
+    discount,
+    status
+FROM {{ source('greenery', 'promos') }}
